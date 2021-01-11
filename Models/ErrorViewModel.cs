1b5626d36fe7c4b0dc48125e78cd54902a3e99b0
Juan <juanviana2577@gmail.com>
2021-01-11 20:20:57 -0300

    Teste Finalizado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC_Aspnet_Core_Razor.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
